f2bd29cfc6c353df1c1a76f244d7bbb1c78c9834
Jesse <jessedrebin@gmail.com>
2018-04-05 21:45:00 -0700

    add if else statements for beep boop and sorry

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var arrayNumbers = [];
 function bunchaNumbers(number) {
-  arrayNumbers = [];
+  // arrayNumbers = [];
   for (var i = 0; i <= number; i++) {
     arrayNumbers.push(i.toString());
 
@@ -10,7 +10,19 @@ function bunchaNumbers(number) {
 arrayNumbers.forEach(function(string){
   var numberedString = parseINt(string);
   var splitNumberedString = string.split("")
-})
+  if (numberedString % 3 === 0 && numberedString != 0) {
+    arrayNumbers.splice("I'm sorry Dave. I'm afraid I can't do that.");
+  } else if (splitNumberedString.indexOf("1") > 0) {
+    arrayNUmbers.splice(string, 1, "Boop");
+  } else if (splitNumberedString.indexOf("0") > 0) {
+    arrayNumbers.splice(string, 1, "Beep");
+  }
+});
+
+
+
+
+
 $(document).ready(function() {
   $("form#beep-boop").submit(function(event) {
     event.preventDefault();
