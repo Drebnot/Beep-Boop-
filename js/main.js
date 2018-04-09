37e777e3846ed877553c0c73800be336d225d776
Jesse <jessedrebin@gmail.com>
2018-04-08 23:12:17 -0700

    add zero number variable

--- js/main.js
+++ js/main.js
@@ -1,23 +1,28 @@
 var arrayNumbers = [];
 function bunchaNumbers(number) {
-  // arrayNumbers = [];
+  var zeroNumber = 0;
   for (var i = 0; i <= number; i++) {
-    arrayNumbers.push(i.toString());
-
+    arrayNumbers.push(""+zeroNumber);
+    zeroNumber++;
   }
-  return arrayNumbers;
-}
+ arrayNumbers.push(""+number);
+;
 arrayNumbers.forEach(function(string){
-  var numberedString = parseINt(string);
-  var splitNumberedString = string.split("")
+  var numberedString = parseInt(string);
+  var splitNumberedString = string.split("");
   if (numberedString % 3 === 0 && numberedString != 0) {
     arrayNumbers.splice("I'm sorry Dave. I'm afraid I can't do that.");
   } else if (splitNumberedString.indexOf("1") > 0) {
-    arrayNUmbers.splice(string, 1, "Boop");
+    arrayNumbers.splice(string, 1, "Boop");
   } else if (splitNumberedString.indexOf("0") > 0) {
     arrayNumbers.splice(string, 1, "Beep");
   }
+
 });
+
+return arrayNumbers;
+
+}
 
 
 
